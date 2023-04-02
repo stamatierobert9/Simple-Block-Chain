dd1245f4b9b9f5745467402465946c5eb1347018
stamatierobert9 <91949766+stamatierobert9@users.noreply.github.com>
2023-04-02 23:26:56 +0300

    Add files via upload

---
+++ Simple Blockchain/src/Block.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+public class Block {
+
+	public String hash;
+	public String previousHash;
+	private String data; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+
+	private int nonce;
+
+	//Block Constructor.
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		this.hash= CalculateHash();
+	}
+
+	public String CalculateHash(){
+		String calculateHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+		return calculateHash;
+	}
+
+	public void mineBlock(int difficulty){
+		String target = new String(new char[difficulty]).replace('\0', '0');
+		while (!hash.substring(0, difficulty).equals(target)) {
+			nonce++;
+			hash = CalculateHash();
+		}
+		System.out.println("Block Mined : " + hash);
+	}
+}
